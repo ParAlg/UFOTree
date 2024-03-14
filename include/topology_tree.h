da048b2b14cc67cbeff89e844e63529a589d4047
Quinten De Man <quinten.deman@gmail.com>
2024-03-14 16:42:35 -0400

    started topology tree update

--- include/topology_tree.h
+++ include/topology_tree.h
@@ -12,17 +12,58 @@ struct TopologyCluster {
 template<typename aug_t>
 class TopologyTree {
 public:
-    TopologyTree(int n, QueryType query_type, std::function<aug_t(aug_t, aug_t)> f);
-    void link(int u, int v, aug_t value = 0);
-    void cut(int u, int v);
+    // Topology Tree interface
+    TopologyTree(vertex_t n, QueryType query_type, std::function<aug_t(aug_t, aug_t)> f);
+    void link(vertex_t u, vertex_t v, aug_t value = 0);
+    void cut(vertex_t u, vertex_t v);
 private:
+    // Class data and parameters
     parlay::sequence<TopologyCluster> leaves;
     std::function<aug_t(aug_t, aug_t)> f;
     QueryType query_type;
+    // Helper functions
+    void insert_neighbor(TopologyCluster<aug_t>* u, TopologyCluster<aug_t>* v);
+    void remove_neighbor(TopologyCluster<aug_t>* u, TopologyCluster<aug_t>* v)
 };
 
 template<typename aug_t>
-TopologyTree<aug_t>::TopologyTree(int n, QueryType q, std::function<aug_t(aug_t, aug_t)> f) :
+TopologyTree<aug_t>::TopologyTree(vertex_t n, QueryType q, std::function<aug_t(aug_t, aug_t)> f) :
 query_type(query_type) f(f) { leaves.reserve(n); }
 
+template<typename aug_t>
+void TopologyTree<aug_t>::link(vertex_t u, vertex_t v, aug_t value) {
+    // remove_ancestors(u, v);
+    insert_neighbor(&leaves[u], &leaves[v]);
+    insert_neighbor(&leaves[v], &leaves[u]);
+    // recluster(u, v);
+}
 
+template<typename aug_t>
+void TopologyTree<aug_t>::cut(vertex_t u, vertex_t v) {
+    // remove_ancestors(u, v);
+    remove_neighbor(&leaves[u], &leaves[v]);
+    remove_neighbor(&leaves[v], &leaves[u]);
+    // recluster(u, v);
+}
+
+template<typename aug_t>
+void TopologyTree<aug_t>::insert_neighbor(TopologyCluster<aug_t>* u, TopologyCluster<aug_t>* v) {
+    for (int i = 0; i < 3; i++)
+        if (leaves[u]->neighbors[i] == nullptr) {
+            leaves[u]->neighbors[i] = leaves[v];
+            return;
+        }
+    std::cerr << "No space to insert neighbor." << std::endl;
+    std::abort();
+}
+
+template<typename aug_t>
+void TopologyTree<aug_t>::remove_neighbor(TopologyCluster<aug_t>* u, TopologyCluster<aug_t>* v) {
+    for (int i = 0; i < 3; i++)
+        if (leaves[u]->neighbors[i] == leaves[v]) {
+            leaves[u]->neighbors[i] = nullptr;
+            return;
+        }
+    std::cerr << "Neighbor to delete not found." << std::endl;
+    std::abort();
+}
