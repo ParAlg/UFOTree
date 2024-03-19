0790974fabbfd1bd00977bc177cae524f6c00574
Quinten De Man <quinten.deman@gmail.com>
2024-03-19 12:29:54 -0400

    topology update: added decremental test, fails on larger trees

--- test/topology_tree_tests.cpp
+++ test/topology_tree_tests.cpp
@@ -3,31 +3,49 @@
 
 
 TEST(TopologyTreeSuite, incremental_linkedlist_test) {
-    vertex_t n = 1024;
+    vertex_t n = 256;
     QueryType qt = PATH;
     auto f = [](int x, int y)->int{return x + y;};
-
     TopologyTree<int> tree(n, qt, f);
+
     for (vertex_t i = 0; i < n-1; i++) {
         tree.link(i,i+1);
-    }
-    for (vertex_t u = 0; u < n-1; u++)
-        for (vertex_t v = u+1; v < n; v++)
+        for (vertex_t u = 0; u < i+1; u++) for (vertex_t v = u+1; v <= i+1; v++)
             ASSERT_TRUE(tree.connected(u,v)) << "Vertex " << u << " and " << v << " not connected.";
+    }
 }
 
 TEST(TopologyTreeSuite, incremental_binarytree_test) {
-    vertex_t n = 1024;
+    vertex_t n = 256;
     QueryType qt = PATH;
     auto f = [](int x, int y)->int{return x + y;};
-
     TopologyTree<int> tree(n, qt, f);
+
     for (vertex_t i = 0; i < (n-1)/2; i++) {
         tree.link(i,2*i+1);
         tree.link(i,2*i+2);
+        for (vertex_t u = 0; u < 2*i+2; u++) for (vertex_t v = u+1; v <= 2*i+2; v++)
+            ASSERT_TRUE(tree.connected(u,v)) << "Vertex " << u << " and " << v << " not connected.";
     }
     if (n%2 == 0) tree.link((n-1)/2,n-1);
-    for (vertex_t u = 0; u < n-1; u++)
-        for (vertex_t v = u+1; v < n; v++)
+    for (vertex_t u = 0; u < n-1; u++) for (vertex_t v = u+1; v < n; v++)
             ASSERT_TRUE(tree.connected(u,v)) << "Vertex " << u << " and " << v << " not connected.";
+}
+
+TEST(TopologyTreeSuite, decremental_linkedlist_test) {
+    vertex_t n = 8;
+    QueryType qt = PATH;
+    auto f = [](int x, int y)->int{return x + y;};
+    TopologyTree<int> tree(n, qt, f);
+
+    for (vertex_t i = 0; i < n-1; i++) {
+        tree.link(i,i+1);
+    }
+    for (vertex_t i = 0; i < n-1; i++) {
+        tree.cut(i,i+1);
+        for (vertex_t u = 0; u < i+1; u++) for (vertex_t v = u+1; v < n; v++)
+            ASSERT_FALSE(tree.connected(u,v)) << "Vertex " << u << " and " << v << " connected.";
+        for (vertex_t u = i+1; u < n-1; u++) for (vertex_t v = u+1; v < n; v++)
+            ASSERT_TRUE(tree.connected(u,v)) << "Vertex " << u << " and " << v << " not connected.";
+    }
 }
