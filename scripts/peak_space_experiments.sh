a8e953e863017b88c50ebdc9ec6d1f60e9f6cbb5
Quinten De Man <quinten.deman@gmail.com>
2024-09-15 16:25:51 -0400

    peak space reproducability script

---
+++ scripts/peak_space_experiments.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare base_dir="$(dirname $(dirname $(realpath $0)))"
+cd ${base_dir}
+
+mkdir -p build
+mkdir -p results
+
+cd ${base_dir}/build
+set -e
+cmake ..
+make -j
+set +e
+
+n_list=(1000 10000 100000)
+./benchmark_peak_space "${n_list[@]}"
+
+cd ${base_dir}/scripts
+for n in "${n_list[@]}"; do
+    python3 plot_results.py ../results/peak_space_$n.csv ../results/peak_space_$n.pdf
+done
