f58ccc49cd826e3e21ebdc02b931744ef3ee030d
happyrogue2307 <atharvaandmonali@gmail.com>
2024-11-04 14:49:04 -0500

    Finished Top Tree Link

--- include/top_tree.h
+++ include/top_tree.h
@@ -7,8 +7,10 @@ template <typename aug_t>
 class TopTree {
     public:
         struct tree t;
-        TopTree(size_t n, QueryType q, std::function<aug_t(aug_t,aug_t)> f, aug_t id, aug_t d_val);
+        TopTree(size_t n, QueryType q = PATH, std::function<aug_t(aug_t,aug_t)> f = [] (int a, int b){return std::min(a,b);}, 
+                aug_t id = std::numeric_limits<int>::max(), aug_t d_val = std::numeric_limits<int>::max());
         void link(vertex_t u, vertex_t v, aug_t weight);
+        bool connected(vertex_t u, vertex_t v);
         void cut(vertex_t u, vertex_t v);
 };
 
@@ -18,14 +20,26 @@ TopTree<aug_t>::TopTree(size_t n, QueryType q, std::function<aug_t(aug_t, aug_t)
 }
 
 template <typename aug_t>
-void TopTree<aug_t>::link(vertex_t u, vertex_t v, aug_t weight){
-   link(&t.vertices[u], &t.vertices[v], weight);
+bool TopTree<aug_t>::connected(vertex_t u, vertex_t v){
+    tt_node* root1 = expose(&t.vertices[u]);
+    tt_node* root2 = expose(&t.vertices[v]);
+    auto to_ret = root1 && root1 == root2;
+    deexpose(&t.vertices[u]);
+    deexpose(&t.vertices[v]);
+    return to_ret;
+}
+template <typename aug_t>
+void TopTree<aug_t>::link(vertex_t u, vertex_t v, aug_t weight)
+{
+    struct vertex *ui = &t.vertices[u];
+    struct vertex *vi = &t.vertices[v];
+    tt_link(ui, vi, weight);
 }
 
-//void TopTree<aug_t>::cut
+// void TopTree<aug_t>::cut
 
 // Given 2 vertices, call expose - traverse down the tree for each tt_node that has u and v as boundary vertices. Keep this going until tt_node->leaf = true.
 template <typename aug_t>
-void TopTree<aug_t>::cut(vertex_t u, vertex_t v){
-    
+void TopTree<aug_t>::cut(vertex_t u, vertex_t v)
+{
 }
--- test/top_tree_tests.cpp
+++ test/top_tree_tests.cpp
@@ -9,3 +9,33 @@ TEST(Top_tree_suite, constructor_test){
     TopTree<int> t(3, PATH, [] (int a, int b){return std::min(a,b);}, std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
     ASSERT_EQ(t.t.num_vertices, 3);
 }
+
+TEST(Top_tree_suite, basic_link_test){
+    TopTree<int> t(10, PATH, [] (int a, int b){return std::min(a,b);}, std::numeric_limits<int>::max(), std::numeric_limits<int>::max());
+    for(int i = 1; i < 9; i++) t.link(i,i+1, i);
+    for(int i = 1; i < 9 ; i++) {
+        for(int j = i + 1; j < 9; j++){
+            ASSERT_TRUE(t.connected(i,j));
+        }
+    }
+}
+
+TEST(Top_tree_suite, testInsertDeleteRakeLinkedList){
+  std::vector<vertex_t> to_test({2, 10, 100, 1000});
+  for(auto llist_size : to_test){
+    TopTree<int> tree(llist_size);
+    for(int i = 0; i < llist_size - 1; i++){
+        tree.link(i , i + 1, i + 1); 
+    }
+    
+    for(int i = 0; i < llist_size - 1; i++){
+        for(int j = i + 1; j < llist_size - 1; j++){
+            ASSERT_TRUE(tree.connected(i,j));
+        }
+    }
+    /*for(int i = 0; i < llist_size - 1; i++){
+      tree.cut(i , i + 1);
+      ASSERT_FALSE(tree.connected(i, i+1));
+    }*/
+  }
+}
